4b51864349c0e6988bb1eb51b7c06666d1cf474b
Fahad-smto <simanto.smto.prog@gmail.com>
2026-04-20 22:54:39 +0600

    added

--- src/generics.ts
+++ src/generics.ts
@@ -11,3 +11,19 @@ let bool = identity<boolean>(true);  // টাইপ boolean
 console.log(num); // 100
 console.log(str); // Hello
 console.log(bool); // true
+
+// 2
+
+
+// একটি Generic ফাংশন যা array-এর প্রথম উপাদান রিটার্ন করে
+function getFirstElement<T>(arr: T[]): T | undefined {
+    return arr[0];
+}
+
+const numbers = [10, 20, 30];
+const firstNumber = getFirstElement(numbers); // টাইপ: number
+console.log(firstNumber); // 10
+
+const names = ["Rahim", "Karim", "Fatema"];
+const firstName = getFirstElement(names); // টাইপ: string
+console.log(firstName); // Rahim
