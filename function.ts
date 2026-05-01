ee1257bc4bd737e2126a1f44f96600d12e3ab37b
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-02 00:41:32 +0600

    arrow function added

--- function.ts
+++ function.ts
@@ -6,3 +6,11 @@ const result= add(2,"2")
 
 console.log(result);
 
+
+const addArrow = (num1:number,num2:number):number=>{
+return (num1+num2)
+}
+
+const result1 = addArrow(4,4);
+
+console.log(result1);
