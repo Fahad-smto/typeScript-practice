4e6fea199912c80d340f058728dfcac664862b64
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-04 23:48:13 +0600

    added

---
+++ src/non-primitive.ts
@@ -0,0 +1,21 @@
+let playlist:string[] =["football","pubg","lodo"];
+
+playlist.push("cricket");
+
+// console.log(playlist);
+
+
+const user :{
+    firstName:string;
+    lastName:string;
+    ismarried:boolean;
+    children:number
+}={
+    firstName:'kalu',
+    lastName:'mia',
+    ismarried:true,
+    children:2
+
+}
+
+console.log(user);
