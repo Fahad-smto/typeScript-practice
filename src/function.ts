31c008ba8cbcbc900a967f41bcbaa2bfdf648558
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-19 23:15:19 +0600

    added

---
+++ src/function.ts
@@ -0,0 +1,53 @@
+function add (num1:number,num2:string){
+    return (num1+num2);
+};
+
+const result= add(2,"2")
+
+console.log(result);
+
+
+const addArrow = (num1:number,num2:number):number=>{
+return (num1+num2)
+}
+
+const result1 = addArrow(4,4);
+
+console.log(result1);
+
+
+const user ={
+    name : 'fahad',
+    balance:0,
+    addBalance(value:number):number{
+        const totalBalance = this.balance+ value;
+        return totalBalance;
+    }
+}
+const addBalance1 =user.addBalance(10000000)
+
+console.log(addBalance1);
+
+
+
+const arr : number [] = [ 1,3,4,5,3];
+
+const squrArray = arr.map((elem:number):number=>
+elem*elem
+)
+
+console.log(squrArray)
+
+
+
+const poorUser ={
+    name:'lalu',
+    balance:5,
+    addBalance(value:number):number{
+        const totalBalance =this.balance + value;
+        return totalBalance;
+    },
+};
+
+poorUser.addBalance(10000);
+console.log(poorUser.addBalance(10000));
